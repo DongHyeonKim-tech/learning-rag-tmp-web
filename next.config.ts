0c22a2b2b75794d160e903a9eed1a415fa5d90a7
DongHyeonKim-tech <sxiloveixz00@nate.com>
2026-04-07 14:36:47 +0900

    image url 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/search",
+  images: {
+    domains: ["hub.haeahn.com", "bim-cdn.haeahn.com"],
+  },
 };
 
 export default nextConfig;
