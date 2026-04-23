3aba3eda45fc6ad196e2976a2669b563f947ec43
DongHyeonKim-tech <sxiloveixz00@nate.com>
2026-04-23 11:36:01 +0900

    Pretendard font global 적ㅇ요

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 import CookieProvider from "@/utils/CookieProvider";
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
 
 export const metadata: Metadata = {
   title: "BIM Search",
@@ -13,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={pretendard.variable}>
       <body>
         <CookieProvider>{children}</CookieProvider>
       </body>
